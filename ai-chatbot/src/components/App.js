c9ecbdd1834b1456a2c98ac0f2c9ef8f5a8b6d97
Beshwanthsai <beshwanthsai2005@gmail.com>
2025-03-24 10:48:35 +0530

    added a simple nav bar that navigates to login and signup pages accordingly

--- ai-chatbot/src/components/App.js
+++ ai-chatbot/src/components/App.js
@@ -6,15 +6,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
 import Landing from './Landing';
+import { Navbar } from './Landing';
 
 function App() {
     return (
+        
         <Container className='d-flex align-items-center justify-content-center mt-5 '
             style={{ minHeight: '100vh' }}>
+            <Navbar />
             <div className='w-100' style={{ maxWidth: '400px' }}>
+            
                 <Router>
                     <AuthProvider>
                         <Routes>
+                            
                             <Route path="/" element={<Landing />} />
                             <Route path="/Dashboard" element={<Dashboard />} />
                             <Route path="/signup" element={<SignUp />} />
--- ai-chatbot/src/components/Landing.js
+++ ai-chatbot/src/components/Landing.js
@@ -3,29 +3,45 @@ import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
+export function Navbar() {
+    return (
+        <nav className='nav m-4' style={{ backgroundColor: "yellowgreen", borderRadius: "10px", textAlign: "center", padding: "10px", flex: "1" }}>
+            <a href='/' className='chat-bot-title m-3 ' > Chat Bot </a>
+            <ul className='m-3' style={{ display: "flex", justifyContent: "center" , gap: "100px"}}>
+                <li>
+                    <a href="/login">Log In</a>
+                </li>
+                <li>
+                    <a href="/signup">Sign Up</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+
 export default function Landing() {
     return (
         <>
-        <div >
+            <div >
+                <center>
+                    <div className='mb-5' style={{ backgroundColor: "yellowgreen", borderRadius: "15px" }}>Landing Page</div>
+                </center>
+                <Card style={{ backgroundColor: "lightblue" }} className='mb-5 w-100 text-center'>
+                    Hi Hello Welcome to Landing Page
 
-            <center>
-                <div className='mb-5' style={{ backgroundColor: "yellowgreen", borderRadius: "15px" }}>Landing Page</div>
-            </center>
-            <Card style={{ backgroundColor: "lightblue" }} className='mb-5 w-100 text-center'>
-                Hi Hello Welcome to Landing Page
-
-            </Card>
-            <div className="w-100 text-center mt-2">
-                Get Started <Link to={"/login"}>Log In</Link>
-            </div>
+                </Card>
+                <div className="w-100 text-center mt-2">
+                    Get Started <Link to={"/login"}>Log In</Link>
+                </div>
                 <br /><br />
-            <p>HI Hello guys if you are not having an account? SignUp to ChatBot</p>
-            <div className='mt-10'>
-                <div className="w-100 text-center mt-6">
-                    Need an account? <Link to={"/signup"}>SignUp</Link>
+                <p>HI Hello guys if you are not having an account? SignUp to ChatBot</p>
+                <div className='mt-10'>
+                    <div className="w-100 text-center mt-6">
+                        Need an account? <Link to={"/signup"}>SignUp</Link>
+                    </div>
                 </div>
             </div>
-        </div>
 
         </>
     )
